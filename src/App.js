d8a0ed514120aec32bee2ce7a217b5ee70c0efeb
Yaroslav Losev <losevyaroslav@gmail.com>
2018-04-05 13:34:22 +0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
 
-import RepositoryInput from './components/RepositoryInput/RepositoryInput';
 import IssuesList from './components/IssuesList/IssuesList';
-import GithubAPI from "./api/GithubAPI";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            issues: []
-        };
-    }
-
-    fetchIssues = (path) => {
-        GithubAPI.fetchIssues(path).then((items) =>{
-            this.setState({issues: items});
-        });
-    };
-
     render() {
-        const testItems = this.state.issues;
-
         return (
             <div>
-                <RepositoryInput handleSubmit={this.fetchIssues}/>
-                <IssuesList items={testItems}/>
+                <IssuesList/>
             </div>
         );
     }
--- src/components/IssuesList/IssuesList.jsx
+++ src/components/IssuesList/IssuesList.jsx
@@ -1,19 +1,35 @@
 import React, {Component} from 'react';
+
+import RepositoryInput from './../RepositoryInput/RepositoryInput';
 import IssuesListItem from './IssuesListItem';
+import GithubAPI from "../../api/GithubAPI";
 
 
 class IssuesList extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {items: []};
     }
 
+
+    loadData = (path) => {
+        GithubAPI.fetchIssues(path).then((items) => {
+            this.setState({items: items});
+        });
+    };
+
+
     render() {
-        const items = this.props.items.map((item) => <IssuesListItem key={item.id} item={item}/>);
+        const items = this.state.items.map((item) => <IssuesListItem key={item.id} item={item}/>);
 
         return (
-            <ul>
-                {items}
-            </ul>
+            <div>
+                <RepositoryInput handleSubmit={this.loadData}/>
+                <ul>
+                    {items}
+                </ul>
+            </div>
         );
     }
 }
